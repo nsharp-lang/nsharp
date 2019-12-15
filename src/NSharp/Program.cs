9d5d2af8db1b8349d06381d16aa5da5b8bc83e3a
deinok <deinok@deinok.com>
2019-12-15 22:12:30 +0100

    RootCommand

---
+++ src/NSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace NSharp {
+
+    public class Program{
+
+        public static async Task<int> Main(string[] args){
+            var rootCommand = new RootCommand { };
+            return await rootCommand.InvokeAsync(args);
+        }
+
+    }
+
+}
