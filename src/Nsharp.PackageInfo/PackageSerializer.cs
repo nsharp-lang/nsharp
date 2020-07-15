2d081b5d0514c9c8ed88558663eafc27ca422e23
Raul Hidalgo Caballero <deinok@deinok.com>
2020-07-15 06:38:10 +0200

    Update PackageSerializer.cs

--- src/Nsharp.PackageInfo/PackageSerializer.cs
+++ src/Nsharp.PackageInfo/PackageSerializer.cs
@@ -1,4 +1,7 @@
+using System.IO;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Nsharp.PackageInfo {
 
@@ -9,6 +12,15 @@ namespace Nsharp.PackageInfo {
 
 		public Package Deserialize(string json) {
 			return JsonSerializer.Deserialize<Package>(json, jsonSerializerOptions);
+		}
+
+		public async ValueTask<Package> DeserializeAsync(FileInfo fileInfo, CancellationToken cancellationToken = default) {
+			await using var fileStream = fileInfo.OpenRead();
+			return await JsonSerializer.DeserializeAsync<Package>(fileStream, jsonSerializerOptions, cancellationToken);
+		}
+
+		public async ValueTask<Package> DeserializeAsync(Stream utf8Json, CancellationToken cancellationToken = default) {
+			return await JsonSerializer.DeserializeAsync<Package>(utf8Json, jsonSerializerOptions, cancellationToken);
 		}
 
 	}
