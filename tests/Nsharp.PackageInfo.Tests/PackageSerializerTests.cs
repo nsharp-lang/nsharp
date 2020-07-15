b2f930a3125a2de1a1f553e436c96963311ed280
Raul Hidalgo Caballero <deinok@deinok.com>
2020-07-16 01:15:13 +0200

    .

--- tests/Nsharp.PackageInfo.Tests/PackageSerializerTests.cs
+++ tests/Nsharp.PackageInfo.Tests/PackageSerializerTests.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Nsharp.PackageInfo.Tests {
@@ -6,13 +10,35 @@ namespace Nsharp.PackageInfo.Tests {
 
 		[Fact]
 		public void DeserializeFact1() {
-			var package = PackageSerializer.Deserialize(@"
+			var jsonString = @"
 				{
 					""name"": ""fact-1"",
 					""type"": ""library"",
 					""version"": ""1.0.0-alpha3""
 				}
-			");
+			";
+			var package = PackageSerializer.Deserialize(jsonString);
+			Assert.Empty(package.Validate());
+		}
+
+		[Fact]
+		public async Task DeserializeFact2() {
+			var jsonString = @"
+				{
+					""name"": ""fact-1"",
+					""type"": ""library"",
+					""version"": ""1.0.0-alpha3""
+				}
+			";
+			await using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+			var package = await PackageSerializer.DeserializeAsync(memoryStream);
+			Assert.Empty(package.Validate());
+		}
+
+		[Fact]
+		public async Task DeserializeFact3() {
+			var fileInfo = new FileInfo($"{AppContext.BaseDirectory}1.nsharp.json");
+			var package = await PackageSerializer.DeserializeAsync(fileInfo);
 			Assert.Empty(package.Validate());
 		}
 
