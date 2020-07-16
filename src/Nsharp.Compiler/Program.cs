c585646e97a97e9f4b21d752bda10b2942e1347d
Raul Hidalgo Caballero <deinok@deinok.com>
2020-07-16 05:24:08 +0200

    .

---
+++ src/Nsharp.Compiler/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Nsharp.Compiler {
+
+	public class Program {
+
+		public static async Task Main(string[] args) {
+			Console.WriteLine("Hello World!");
+		}
+
+	}
+
+}
---
+++ src/Nsharp.Interpreter/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Nsharp.Interpreter {
+
+	public class Program {
+
+		public static async Task Main(string[] args) {
+			Console.WriteLine("Hello World!");
+		}
+
+	}
+
+}
