03d26acef987f18e24fa25e58d3f8963990aa5a1
deinok <deinok@deinok.com>
2020-09-06 16:14:37 +0200

    .

---
+++ src/Nsharp.AbstractSyntaxTree/Internal/NsharpTokenizer.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace Nsharp.AbstractSyntaxTree.Internal {
+
+	internal static class NsharpTokenizer {
+
+		public static async IAsyncEnumerable<string> TokenizeAsync(TextReader textReader) {
+			yield return null;
+		}
+
+	}
+
+}
--- src/Nsharp.AbstractSyntaxTree/SyntaxTree.cs
+++ src/Nsharp.AbstractSyntaxTree/SyntaxTree.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Nsharp.AbstractSyntaxTree.Internal;
 
 namespace Nsharp.AbstractSyntaxTree {
 
 	public class SyntaxTree {
 
 		public static SyntaxTree Parse(string text) {
+			return null;
+		}
+
+		public static SyntaxTree Parse(Stream stream) {
+			return null;
+		}
+
+		public static async ValueTask<SyntaxTree> ParseAsync(TextReader textReader, CancellationToken cancellationToken = default) {
+			var tokens = NsharpTokenizer.TokenizeAsync(textReader);
 			return null;
 		}
 
